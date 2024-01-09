c5cc99c9bd3eb4c5c092b63eb1ee498fd21c0f91
TanKittipat <kittipat12tan@gmail.com>
2024-01-09 15:45:24 +0700

    2nd

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let queue = [];
+const maxQueueSize = 7;
 
 document.getElementById("enqueueBtn").addEventListener("click", () => {
   const customerName = document.getElementById("customerName").value;
-  if (customerName) {
-    queue.push(customerName);
-    document.getElementById("customerName").value = ""; // Clear the input
-    updateQueueDisplay();
+  if (queue.length < maxQueueSize) {
+    if (customerName) {
+      queue.push(customerName);
+      document.getElementById("customerName").value = ""; // Clear the input
+      updateQueueDisplay();
+    }
+  } else {
+    alert("Queue is Full");
   }
 });
 
@@ -18,9 +23,20 @@ document.getElementById("dequeueBtn").addEventListener("click", () => {
   }
 });
 
+document.getElementById("cls").addEventListener("click", () => {
+  if (queue.length > 0) {
+    queue.length = 0;
+    alert("Queue Cleared!!!");
+    updateQueueDisplay();
+  } else {
+    alert("No more customers in the queue.");
+  }
+});
+
 function updateQueueDisplay() {
   const queueList = document.getElementById("queueList");
-  queueList.innerHTML = "<h3>Queue</h3>";
+  queueList.innerHTML =
+    "<h3>Queue : " + queue.length + " / " + maxQueueSize + "</h3>";
   queue.forEach((customer, index) => {
     queueList.innerHTML += `<p>${index + 1}. ${customer}</p>`;
   });
